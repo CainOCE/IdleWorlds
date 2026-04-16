e716466c0c92903978f08b43e3d3ed5c5045af4e
Cain Bruhn-Tanzer <cainbt@outlook.com>
2026-04-17 08:19:27 +1000

    More Structuring

--- app/Application.js
+++ app/Application.js
@@ -1,16 +1,10 @@
-import { App } from "/app/Application.js"
 
 // register service worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/pwa-service-worker.js');
 }
 
+console.log("Hello World!")
+
 /* Choose some Modules to import */
 let mods = ["/base/master_manifest.json"]
-
-/* Start App w/ Game */
-const session = new App();
-await session.init();
-session.run_game();
-
-console.log("Hello World!")
